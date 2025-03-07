03c576bf2199b04237ab0e6adf361c67a9b95867
Pauuu12344 <137855342+Pauuu12344@users.noreply.github.com>
2025-03-06 21:08:47 -0600

    Agregar

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request,redirect
 from flask import flash 
 from flask_wtf.csrf import CSRFProtect
 from config import DevelopmentConfig
 from flask import  g
 import forms
+
 from models import db
 from models import Alumnos
 
@@ -14,7 +15,35 @@ csrf=CSRFProtect()
 @app.route("/")
 @app.route("/index")
 def index():
-	return render_template("index.html")
+	create_form= forms.UserForm2(request.form)
+	alumno=Alumnos.query.all()
+	return render_template("index.html",form=create_form,alumnos=alumno)
+
+@app.route("/detalles",methods=['GET','POST'])
+def detalles():
+	create_form=forms.UserForm2(request.form)
+	if request.method=='GET':
+		id=request.args.get('id')
+		alum1 = db.session.query(Alumnos).filter(Alumnos.id==id).first()
+		id=request.args.get('id')
+		nombre=alum1.nombre
+		apaterno=alum1.apaterno
+		email=alum1.email
+
+	return render_template('detalles.html',id=id,nombre=nombre,apaterno=apaterno,email=email)
+
+@app.route("/Alumnos1",methods=['GET','POST'])
+def alumnos1():
+	create_form=forms.UserForm2(request.form)
+	if request.method=='POST':
+		alumn=Alumnos(nombre=create_form.nombre.data,
+		apaterno=create_form.apaterno.data,
+		email=create_form.email.data)
+		db.session.add(alumn)
+		db.session.commit()
+		return redirect(url_for('index'))
+
+	return render_template('Alumnos1.html',form=create_form)
 
 if __name__ == '__main__':
 	csrf.init_app(app)
